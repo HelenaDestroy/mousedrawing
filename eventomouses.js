6f35f96710d7422ae56d62e72dd4cdf2454e219e
Jessica Torti <jesicatorti@gmail.com>
2022-03-29 06:29:36 -0300

    not working but still not working.

--- eventomouses.js
+++ eventomouses.js
@@ -1,54 +1,53 @@
 document.getElementById("area_de_dibujo");
-var cuadrito = document.getElementById("area_de_dibujo");
-var papel = cuadrito.getContext("2d");
+let cuadrito = document.getElementById("area_de_dibujo");
+let papel = cuadrito.getContext("2d");
 document.getElementById("rainbow");
 document.getElementById("botoncito");
-botoncito.addEventListener("click", aviso);
 var colorin = document.getElementById("rainbow");
-var x = 0;
-var y = 0;
+let x = 0;
+let y = 0;
 estado = 0;
-
-function aviso()
-{     alert("Color " + colorin.value + " seleccionado.");  }   
 
 area_de_dibujo.addEventListener("mousedown", pressMouse);
 area_de_dibujo.addEventListener("mousemove", moveMouse);
 area_de_dibujo.addEventListener("mouseup", leaveMouse);
 
-function pressMouse(evento)
+function pressMouse(estado)
 {
 estado = 1;
-var x = evento.layerX;
-var y = evento.layerY;
+x = papel.layerX;
+y = papel.layerY;
 }
-function moveMouse (evento)
-{ if (estado == 1) {
-dibujarLinea(colorin.value, x, y, evento.layerX, evento.layerY, papel);
-x = evento.layerX + 0,5;
-y = evento.layerY + 0,5;
-}
-else { 
-  x = evento.layerX;
-  y = evento.layerY;
-}
+function moveMouse (estado)
+{ if (estado == 1) 
+    {
+    dibujarLinea(colorcito, x, y, papel.layerX, evento.layerY, papel);
+    x = papel.layerX;
+    y = papel.layerY;    
+    colorcito = colorin.value;
+    xfinal = papel.screenX + 0.5;
+    yfinal = papel.screenY + 0.5;
+    
+    }
+    else { leaveMouse();
+   }
 }
 
-function leaveMouse(evento)
+function leaveMouse(estado)
  {
-    estado = 0;
+    estado == 0;
  }
  
-function dibujarLinea(color,xinicial,yinicial, xfinal, yfinal, papel)
+function dibujarLinea(colored,xinicial,yinicial, xfinal, yfinal, hoja)
 {
-    var color = colorin.value;
-    var cuadrito = document.getElementById("area_de_dibujo");
-    var papel = cuadrito.getContext("2d");
-    papel.beginPath();
-    papel.strokeStyle = color;
-    papel.lineWidth = 2;
-    papel.moveTo(xinicial, yinicial);
-    papel.lineTo(xfinal, yfinal);
-    papel.stroke();
-    papel.closePath;
+    let colored = colorin.value;
+    let cuadrito = document.getElementById("area_de_dibujo");
+    let hoja = cuadrito.getContext("2d");
+    hoja.beginPath();
+    hoja.strokeStyle = color;
+    hoja.lineWidth = 2;
+    hoja.moveTo(xinicial, yinicial);
+    hoja.lineTo(xfinal, yfinal);
+    hoja.stroke();
+    hoja.closePath;
     }
