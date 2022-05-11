0a6ee8f2c086fbb6d5d63ede32a9227c73be4106
jess <jesicatorti@gmail.com>
2022-05-11 18:59:19 -0300

    finished

---
+++ mousedrawing/README.md
@@ -0,0 +1,4 @@
+# mousedrawing
+Second practice with js, now with mouse drawing.
+You can see the project HERE:
+https://helenadestroy.github.io/mousedrawing/
---
+++ mousedrawing/eventomouses.js
@@ -0,0 +1,44 @@
+let colorin = document.getElementById("rainbow");
+let cuadrito = document.getElementById("area_dibujo");
+cuadrito.addEventListener("mousedown", pressMouse);
+cuadrito.addEventListener("mousemove", moveMouse);
+cuadrito.addEventListener("mouseup", leaveMouse);
+let papel = cuadrito.getContext("2d");
+let estado = 0;
+let x = 0;
+let y= 0;
+
+function pressMouse(evento)
+{
+    // guardar posición
+estado = 1;
+x = evento.clientX;
+y = evento.clientY;
+}
+function leaveMouse(evento)
+ {
+    estado = 0;
+ }
+
+function dibujarLinea(color,xi,yi, xf, yf, papel)
+{
+    papel.beginPath();
+    papel.strokeStyle = color;
+    papel.lineWidth = 2;
+    papel.moveTo(xi, yi);
+    papel.lineTo(xf, yf);
+    papel.stroke();
+    papel.closePath();
+}
+function moveMouse (event)
+{
+    if (estado == 1) {
+    const bounds = cuadrito.getBoundingClientRect();  //valor x e y cuando empieza el canvas.
+   //dibuja línea desde posición guardada previamente a la posición actual.
+    dibujarLinea(colorin.value, x - bounds.left, y - bounds.top, event.clientX - bounds.left, event.clientY - bounds.top, papel);
+    //actualizar posición guardada con la actual.
+    }
+    // guarda posición en x e y ya con el mouse habiéndose movido
+    x = event.clientX;
+    y = event.clientY;
+}
